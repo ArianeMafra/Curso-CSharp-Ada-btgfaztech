b355f34b1cc633ff9bf11a1bad53c984fca5268a
Ariane Mafra <arianecristinamafra@gmail.com>
2023-08-08 19:45:30 -0300

    Adiciona exercicio aula 3

---
+++ Nivelamento LP e POO/Exercicio_aula3/Exercicio_aula3/Produto.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercicio_aula3
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return "Dados do Produto \nNome: " + Nome + ", Preço: R$" + Preco.ToString("F2") + ", Quantidade em Estoque: " + Quantidade +
+                ", Valor total em estoque: R$" + ValorTotalEmEstoque().ToString("F2") + ".";
+        }
+    }
+
+}
---
+++ Nivelamento LP e POO/Exercicio_aula3/Exercicio_aula3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercicio_aula3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre com as informações do produto.");
+
+            Console.Write("Nome do produto: ");
+            produto.Nome = Console.ReadLine();
+
+            Console.Write("Preço do produto: ");
+            produto.Preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade em estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(produto);
+            Console.WriteLine();
+
+            Console.Write("Entre com o número de produtos que serão adicionados ao estoque: ");
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine(produto);
+            Console.WriteLine();
+
+            Console.Write("Entre com o número de produtos que serão removidos do estoque: ");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine(produto);
+
+            Console.ReadKey();
+        }
+    }
+}
