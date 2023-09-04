39bfa2500e0e613ff7d5c22d742c22022a1e11b4
Ariane Mafra <arianecristinamafra@gmail.com>
2023-09-04 11:40:45 -0300

    Adiciona Ex03

---
+++ POO 2/Exercicios-Aula-3008/Ex03/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Exercício 3
+Vamos pegar o projeto de vcs da Empresa com interfaces!
+Crie uma interface EnviarNotificacao com um método void EnviarMensagemCliente.
+Crie uma classe EnviarNotificacaoWhatsApp, que implementa EnviarNotificacao e nela teremos o método EnviarMensagemCliente que printa a notificação: "Estou enviando uma mensagem no WhatsApp do Cliente!";
+
+Dentro da classe Varejo, vamos incluir um private readonly EnviarNotificacaoWhatsApp que é recebido no construtor da classe.
+Após Realizar a venda, utilizaremos o método de EnviarNotificacao..*/
+
+using Ex03;
+
+namespace Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EnviarNotificacaoWhatsApp notificacaoWhatsApp = new EnviarNotificacaoWhatsApp();
+            List<Empresa> listaEmpresas = new List<Empresa>
+            {
+                new Varejo("Loja XV", "12.345.678/0001-90", "Rua XV, 123, Boa Vista, São Paulo - SP", notificacaoWhatsApp),
+
+                new Empreiteira("Águia Empreiteira", "67.345.123/0001-12", "Avenida das Aves, 456, Centro, São Paulo -SP")
+            };
+
+            Console.WriteLine("Empresas: ");
+            foreach(var empresa in listaEmpresas)
+            {
+                if (empresa is IImprimirInformacoes informacoes)
+                {
+                    informacoes.ImprimirInformacoes();
+                }
+                if (empresa is IRealizarVenda venda)
+                {
+                    venda.RealizarVenda();
+                }               
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+
+}
+
---
+++ POO 2/Exercicios-Aula-3008/Ex03/Varejo.cs
@@ -0,0 +1,31 @@
+﻿using Ex03;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    public class Varejo : Empresa, IImprimirInformacoes, IRealizarVenda
+    {
+        private readonly EnviarNotificacaoWhatsApp notificacao;
+        public Varejo(string nome, string cnpj, string endereco, EnviarNotificacaoWhatsApp notificacao) : base (nome, cnpj, endereco)
+        {
+            this.notificacao = notificacao; 
+        }
+        
+        public void ImprimirInformacoes()
+        {
+            Console.WriteLine($"Nome: {Nome}, CNPJ: {Cnpj}, Endereço: {Endereco}");
+        }
+
+        public void RealizarVenda()
+        {
+            Console.WriteLine("Vende produtos.");
+            notificacao.EnviarMensagemCliente();
+        }
+
+
+    }
+}
